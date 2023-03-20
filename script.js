c6568a146d8d26e2a9408745fa6076146ccf8305
Benz <benzmcc@gmail.com>
2023-03-20 11:23:58 +0530

    style sheet is added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function myClick() {
     //document.getElementById("benz").style.display ="none";
     let messAge="welcome to the script world";
-    document.getElementById("new").innerHTML = messAge;
+    document.getElementById("seema").innerHTML = messAge;
     document.getElementById("new").style.visibility = "visible";
     //alert(messAge);
     console.log(messAge);
