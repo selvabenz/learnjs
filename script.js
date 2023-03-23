97e863da7d31c6bbc280b97d820ec34fa6124ac8
Benz <benzmcc@gmail.com>
2023-03-23 14:02:34 +0530

    sigle button to show and hide message

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function myClick() {
     //document.getElementById("benz").style.display ="none";
     let messAge="welcome to the script world";
-    document.getElementById("seema").innerHTML = messAge;
+    document.getElementById("new").innerHTML = messAge;
     document.getElementById("new").style.visibility = "visible";
     //alert(messAge);
     console.log(messAge);
@@ -9,4 +9,19 @@ function myClick() {
 
 function myHide(){
     document.getElementById("new").style.visibility = "hidden";
+}
+
+function buttonchangeText (){
+    let change = document.getElementById("toggle");
+        if (change.value === 'Show')
+            {
+                change.value = 'Hide';
+                change.innerHTML = myClick();
+            }
+            else
+            {
+                change.value = 'Show';
+                change.innerHTML = myHide();
+            }
+            console.log(change);
 }
